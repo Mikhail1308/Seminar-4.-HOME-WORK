aa2476e0c44613b20f05343e65646ac22449947d
Mikhail Nikolaev <armavir1308@mail.ru>
2023-01-27 01:08:24 +0300

    TASK_01. Возведение числа в степень

---
+++ TASK_01/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 1: Напишите цикл, который принимает на вход два числа (A и B)
+// и возводит число A в натуральную степень B
+
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+
+int numberA = ReadInt("Введите число A: ");
+int numberB = ReadInt("Введите степень B: ");
+DegreeNumber(numberA, numberB);
+void DegreeNumber(int A, int B)
+{
+    int result = 1;
+    for (int i = 1; i <= B; i++)
+    {
+        result = result * A;
+    }
+    Console.WriteLine($"Число {A}, возведенное в степень {B} равно {result}");
+}
+int ReadInt(string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+Console.WriteLine();
+
+
