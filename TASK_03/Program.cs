83f21b27627b4e6adc31f66faf1ecc7f9e28e12c
Mikhail Nikolaev <armavir1308@mail.ru>
2023-01-27 19:56:09 +0300

    TASK_03. Вывод на экран элементов массива

---
+++ TASK_03/Program.cs
@@ -0,0 +1,24 @@
+﻿
+// Напишите программу, которая задаёт массив из 8 элементов
+// и выводит их на экран. Сделать через функции
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+
+Console.Write("Введите размер массива: ");
+int[] Mass = new int[int.Parse(Console.ReadLine())];
+
+var random = new Random();
+for (int i = 0; i < Mass.Length; i++)
+{
+    Mass[i] = random.Next(0, 100);
+}
+Console.WriteLine();
+Console.Write("Исходный массив: ");
+Console.Write("[");
+for (int i = 0; i < Mass.Length; i++)
+
+    Console.Write(Mass[i] + " ");
+
+Console.Write("]");
+Console.WriteLine();
